a5c3e062e0bd01b8430aa54371e00dc303fd7fff
Elaine Hamilton <elainehamilton@elaines-mbp.lan>
2020-10-14 22:02:54 -0700

    update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,14 @@
 
     //console.log(data);
 
-var svgWidth = 960;
-var svgHeight = 500;
+var svgWidth = 750;
+var svgHeight = 150;
 
 var margin = {
-    top: 20,
-    right: 40,
-    bottom: 60,
-    left: 100
+    top: 2,
+    right: 2,
+    bottom: 2,
+    left: 2
 };
 
 var width = svgWidth - margin.left - margin.right;
@@ -30,8 +30,47 @@ d3.csv("./assets/data/data.csv").then(data => {
     
     // parse data/cast as numbers
     data.forEach(data => {
-        data.
-    })
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;
+    });
+
+    // create scale functions
+    var xLinearScale = d3.scaleLinear()
+        .domain([5, d3.max(data, d => d.poverty)])
+        .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.healthcare)])
+        .range([height, 0]);
+
+    // create circles
+    var circlesGroup = scatterGroup.selectAll("circle")
+    .data(data)
+    .join("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", "7")
+    .attr("fill", "pink")
+    .attr("opacity", 0.8)
+   // .attr() for state abbreviations inside, need to look up..
+    .attr("stroke", "black")
+    .attr("stroke-width", 1);
+
+    // create axis functions 
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // append  axes to the chart
+    scatterGroup.append("g")
+        .attr("transform", `translate(0, $height})`)
+        .call(bottomAxis);
+
+    scatterGroup.append("g")
+        .call(leftAxis);
+
+
+
+
     
 
 

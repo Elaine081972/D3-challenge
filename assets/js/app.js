6bf049e2d446c6e1a5291681330e5a86d1f63e60
Elaine Hamilton <elainehamilton@elaines-mbp.lan>
2020-10-15 15:50:30 -0700

    update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,14 +4,14 @@
     //console.log(data);
 
 let svgWidth = 750;
-let svgHeight = 450;
+let svgHeight = 500;
 
 let margin = {
-    top: 10,
-    right: 10,
-    bottom: 10,
-    left: 30
-};
+    top: 30,
+    right: 40,
+    bottom: 60,
+    left: 80
+    };
 
 let width = svgWidth - margin.left - margin.right;
 let height = svgHeight - margin.top - margin.bottom;
@@ -20,7 +20,7 @@ let height = svgHeight - margin.top - margin.bottom;
 let svg = d3.select("#scatter")
     .append("svg")
     .attr("width", svgWidth)
-    .attr("heigth", svgHeight);
+    .attr("height", svgHeight);
 
 let scatterGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
@@ -36,7 +36,7 @@ d3.csv("./assets/data/data.csv").then(data => {
 
     // create scale functions
     let xLinearScale = d3.scaleLinear()
-        .domain([5, d3.max(data, d => d.poverty)])
+        .domain([8, d3.max(data, d => d.poverty) + 2])
         .range([0, width])
         .nice();
 
@@ -45,20 +45,7 @@ d3.csv("./assets/data/data.csv").then(data => {
         .range([height, 0])
         .nice();
 
-    // create circles
-    let circlesGroup = scatterGroup.selectAll("circle")
-    .data(data)
-    .join("circle")
-    .attr("cx", d => xLinearScale(d.poverty))
-    .attr("cy", d => yLinearScale(d.healthcare))
-    .attr("r", "7")
-    .attr("fill", "pink")
-    .attr("opacity", 0.8)
-    // .attr() for state abbreviations inside, need to look up..
-    .attr("stroke", "black")
-    .attr("stroke-width", 1);
-    
-    // create axis functions 
+        // create axis functions 
     let bottomAxis = d3.axisBottom(xLinearScale);
     let leftAxis = d3.axisLeft(yLinearScale);
 
@@ -70,17 +57,33 @@ d3.csv("./assets/data/data.csv").then(data => {
     scatterGroup.append("g")
         .call(leftAxis);
 
+
+    // create circles
+    let circlesGroup = scatterGroup.selectAll("circle")
+    .data(data)
+    .join("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", "8")
+    .attr("class", "stateCircle")
+    // .attr("fill", "pink")
+    // .attr("opacity", 0.8)
+    // .attr() for state abbreviations inside, need to look up..
+    // .attr("stroke", "black")
+    // .attr("stroke-width", 1);
+    
+   
     // create axes labels
     scatterGroup.append("text")
-        .attr("transform", "rotate(-90")
+        .attr("transform", "rotate(-90)")
         .attr("y", 0 - margin.left + 5)
-        .attr("x", 0 -(height / 2))
+        .attr("x", 0 - (height / 2))
         .attr("dy", "1em")
         .attr("class", "aText")
         .text("Lacks Healthcare (%)");
 
     scatterGroup.append("text")
-        .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 20})`)
         .attr("class", "aText")
         .text("In Poverty (%)");
 
